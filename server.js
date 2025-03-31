808e11180abe62db91bf3835d5cb26827eedd5b1
Noah <duyngo@panicbear.consulting>
2025-03-31 11:48:49 +0700

    Update feedback content

--- server.js
+++ server.js
@@ -7,15 +7,20 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle custom routes to remove .html extension
 app.get('/:page', (req, res) => {
-  const page = req.params.page;  
-  console.log(req.params)
+  const page = req.params.page;
+
+  if (page === 'tech') {
+    return res.redirect('/');
+  }
+
   if (page.endsWith('.html')) {
     const pageWithoutExtension = page.slice(0, -5); // Remove the .html extension
-    res.redirect(`/${pageWithoutExtension}`);
-  } else {
-    res.sendFile(path.join(__dirname, 'dist', page + '.html'));
+    return res.redirect(`/${pageWithoutExtension}`);
   }
+
+  res.sendFile(path.join(__dirname, 'dist', page + '.html'));
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
